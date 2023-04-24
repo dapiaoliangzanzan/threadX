61df495ceb4cd2ae4f80bb2a7a9ebf0b688f2dc5
huangfukexing <huangfu>
2023-04-24 20:39:43 +0800

    对外接口封装

---
+++ threadX-metrics-server/src/main/java/com/threadx/metrics/server/common/code/CurrencyRequestEnum.java
@@ -0,0 +1,45 @@
+package com.threadx.metrics.server.common.code;
+
+/**
+ * 常规的、通用的状态码定义
+ *
+ * @author huangfu
+ * @date 2022年11月1日18:17:09
+ */
+public enum CurrencyRequestEnum implements IExceptionCode {
+
+    /**
+     * 请求成功的状态码
+     */
+    REQUEST_SUCCESS("000000", "请求成功"),
+    /**
+     * 常规的处理失败的状态码
+     */
+    REQUEST_ERROR("500000", "处理失败");
+
+    /**
+     * 状态码
+     */
+    private final String code;
+
+    /**
+     * 成功信息
+     */
+    private final String message;
+
+    CurrencyRequestEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+
+    @Override
+    public String getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}
---
+++ threadX-metrics-server/src/main/java/com/threadx/metrics/server/controller/MetricsController.java
@@ -0,0 +1,20 @@
+package com.threadx.metrics.server.controller;
+
+import com.threadx.metrics.server.common.annotations.GlobalResultPackage;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * *************************************************<br/>
+ * 指标数据接口<br/>
+ * ************************************************<br/>
+ *
+ * @author huangfu
+ * @date 2023/4/24 20:22
+ */
+@RestController
+@GlobalResultPackage
+@RequestMapping("/metrics")
+public class MetricsController {
+
+}
