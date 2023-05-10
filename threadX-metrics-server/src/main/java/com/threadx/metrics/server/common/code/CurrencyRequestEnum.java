f929eed47344b8c3ac54699336dbbd03ae57563c
huangfuProject <huangfu_project@163.com>
2023-05-10 16:48:19 +0800

    测试提交邮箱

--- threadX-metrics-server/src/main/java/com/threadx/metrics/server/common/code/CurrencyRequestEnum.java
+++ threadX-metrics-server/src/main/java/com/threadx/metrics/server/common/code/CurrencyRequestEnum.java
@@ -4,7 +4,7 @@ package com.threadx.metrics.server.common.code;
  * 常规的、通用的状态码定义
  *
  * @author huangfu
- * @date 2022年11月1日18:17:11
+ * @date 2022年11月1日18:17:12
  */
 public enum CurrencyRequestEnum implements IExceptionCode {
 
