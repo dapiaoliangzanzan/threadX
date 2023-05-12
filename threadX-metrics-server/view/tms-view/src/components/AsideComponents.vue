889538da506883a3ddb4a7f891d2212a24a02b04
皇甫 <huangfu_project@163.com>
2023-05-12 18:17:10 +0800

    布局前端页面

---
+++ threadX-metrics-server/view/tms-view/src/components/AsideComponents.vue
@@ -0,0 +1,64 @@
+<template>
+    <el-menu
+      default-active="1"
+      active-text-color="#ff0000"
+      background-color="#D8D0D0"
+      :collapse="true"
+    >
+      <img src="../assets/logo.png" alt="threadX" class="logoClass">
+      <el-menu-item index="1">
+        
+        
+        <i class="icon iconfont  icon-gongzuotai1 aside-icon-item"></i>
+        <template #title>工作台</template>
+      </el-menu-item>
+
+      <el-menu-item index="2">
+        <i class="icon iconfont icon-icon-project aside-icon-item"></i>
+        <template #title>项目</template>
+      </el-menu-item>
+
+    </el-menu>
+  </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import '../assets/icon/iconfont.css'
+
+export default defineComponent({
+    setup () {
+        return {
+            
+        }
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+    .el-aside{
+        .el-menu {
+            height: 100vh;
+            width: 90px;
+        }
+        .logoClass {
+            width: 90px;
+            height: 90px;
+        }
+
+        .aside-icon-item {
+            color: #ffffff !important; 
+            font-size: 35px !important;
+        }
+
+        .el-menu-item {
+            width: 90px;
+            height: 90px;
+        }
+
+        .is-active {
+            color: #000;
+        }
+        
+    }
+
+</style>
---
+++ threadX-metrics-server/view/tms-view/src/views/MainHome.vue
@@ -0,0 +1,36 @@
+<template>
+      <div class="common-layout">
+    <el-container>
+      <el-aside>
+        <AsideComponents/>
+      </el-aside>
+
+      <el-main>主</el-main>
+    </el-container>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AsideComponents from '@/components/AsideComponents.vue';
+
+export default defineComponent({
+    setup () {
+        
+
+        return {}
+    },
+    components: {
+        AsideComponents
+    }
+})
+</script>
+
+<style scoped>
+    .logoClass {
+        width: 70px;
+        height: 70px;
+        overflow: hidden;
+        object-fit: cover;
+    }
+</style>
