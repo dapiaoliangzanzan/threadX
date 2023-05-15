8a52a6ebc36042d75412c130ddc8b2be46d77f11
皇甫 <huangfu_project@163.com>
2023-05-15 11:16:21 +0800

    增加 内容组件和表头组件

---
+++ threadX-metrics-server/view/tms-view/src/components/TopComponents.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="topClass"> 
+        top
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        
+
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+    .topClass {
+        height: 50px;
+        border-radius: 50px !important;
+        display: flex;
+        justify-content: center;
+        justify-items: center;
+
+        
+    }
+</style>
